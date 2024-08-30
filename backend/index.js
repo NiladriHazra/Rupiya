08c3e85987e631973014a59f2d8bc66076a6c759
Niladri Hazra <151611097+NiladriHazra@users.noreply.github.com>
2024-08-30 13:28:32 +0530

    added env

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,9 @@
 // backend/index.js
+require('dotenv').config()
 const express = require('express');
 const cors = require("cors");
 const rootRouter = require("./routes/index");
-
+const port = 3000
 const app = express();
 
 app.use(cors());
@@ -10,4 +11,4 @@ app.use(express.json());
 
 app.use("/api/v1", rootRouter);
 
-app.listen(3000);
+app.listen(process.env.PORT);
