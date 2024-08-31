494d187a732200f0a70e18b6e04b9bdb5d53716e
Niladri Hazra <151611097+NiladriHazra@users.noreply.github.com>
2024-09-01 02:11:10 +0530

    added

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/", rootRouter);
+app.use("/api/v1", rootRouter);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
