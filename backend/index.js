f99e3f5cd8aab83a0a156c7aaf9e76413ba15af3
Niladri Hazra <151611097+NiladriHazra@users.noreply.github.com>
2024-09-01 02:15:55 +0530

    added

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,10 @@ const port = process.env.PORT || 3000; // Default to 3000 if PORT is not defined
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: 'https://rupiya-six.vercel.app/',
+    credentials: true
+  }));
 app.use(express.json());
 
 app.use("/api/v1", rootRouter);
